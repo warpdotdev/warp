fa419683849bbb0075743aa1e9ba43011a315c04
cephalonaut <matthew.albright@gmail.com>
2026-07-02 23:13:24 -0400

    reformat

--- crates/integration/src/lib.rs
+++ crates/integration/src/lib.rs
@@ -7,5 +7,4 @@ pub mod util;
 
 pub use builder::Builder;
 pub use warp::integration_testing::view_getters;
-pub use warpui_core::integration::AssertionOutcome;
-pub use warpui_core::integration::TestStep;
+pub use warpui_core::integration::{AssertionOutcome, TestStep};
