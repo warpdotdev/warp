978edddf77768977f910e95731213888f29f8731
David Stern <david@warp.dev>
2026-08-13 22:11:54 -0400

    remove unnecessary clone call

--- crates/cloud_objects/src/cloud_object/mod.rs
+++ crates/cloud_objects/src/cloud_object/mod.rs
@@ -660,7 +660,7 @@ impl CloudObjectMetadata {
     /// there are conflicts, so callers should check for conflicts before calling
     /// this.
     pub fn update_revision_from_server(&mut self, server_metadata: &ServerMetadata) {
-        self.revision = Some(server_metadata.revision.clone());
+        self.revision = Some(server_metadata.revision);
         self.last_editor_uid = server_metadata.last_editor_uid.clone();
     }
 
